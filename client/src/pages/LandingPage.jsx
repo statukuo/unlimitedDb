8d2232d255bb2cc0dfbe6fdfddcc2c92c52b78aa
statukuo <dbdstk@gmail.com>
2024-11-15 22:43:23 +0100

    add infinite scroll load on cards

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -6,6 +6,9 @@ import { CardFilter } from "../components/CardFilter";
 import { SidePanel } from "../components/SidePanel";
 import { Grid2 as Grid } from "@mui/material";
 import { BasePage } from "./BasePage";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const PAGINATION = 20;
 
 const Styles = {
   CardContainer: styled(Grid)`
@@ -16,6 +19,7 @@ const Styles = {
 export function LandingPage() {
   const [cardList, setCardList] = useState([]);
   const [filters, setFilters] = useState({});
+  const [currentShowing, setCurrentShowing] = useState(PAGINATION);
 
   useEffect(() => {
     async function fetchData() {
@@ -34,18 +38,31 @@ export function LandingPage() {
         <CardFilter onFilter={updateFilters} />
       </SidePanel>
 
-      <Styles.CardContainer container spacing={0.5} columns={12}>
-        {cardList.map((card, idx) => {
-          return (
-            <Grid item size={{ xs: 12, md: 6, lg: 4, xl: 2 }} key={idx}>
-              <SWUCard
-                key={idx}
-                data={card}
-              />
-            </Grid>
-          );
-        })}
-      </Styles.CardContainer>
+      <InfiniteScroll
+        dataLength={currentShowing} //This is important field to render the next data
+        next={() =>
+          setCurrentShowing(
+            Math.min(currentShowing + PAGINATION, cardList.length)
+          )
+        }
+        hasMore={currentShowing < cardList.length}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <Styles.CardContainer container spacing={0.5} columns={12}>
+          {cardList.slice(0, currentShowing).map((card, idx) => {
+            return (
+              <Grid item size={{ xs: 12, md: 6, lg: 4, xl: 2 }} key={idx}>
+                <SWUCard key={idx} data={card} />
+              </Grid>
+            );
+          })}
+        </Styles.CardContainer>
+      </InfiniteScroll>
     </BasePage>
   );
 }
